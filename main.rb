899d02a0ff2012698e445ebb038cf8d984016f34
jpeyok <josh.peyok@underdogfantasy.com>
2024-08-16 14:01:11 -0400

    updated readme

--- main.rb
+++ main.rb
@@ -6,17 +6,16 @@ class GpsCLI < Thor
     def csv_headers
       'FILENAME,EXIF_EXISTS,GPS_EXISTS,LATITUDE,LONGITUDE'
     end
-  end
 
-  desc 'extract [FILENAME]', 'Extracts and prints info about a file'
-  def extract(f)
-    data = EXIFR::JPEG.new(f)
-    filename = File.basename(f)
-    e = data.exif?
-    g = !data.gps.nil?
-    lat = data.gps.latitude if data.exif? && !data.gps.nil?
-    long = data.gps.longitude if data.exif? && !data.gps.nil?
-    "#{filename},#{e},#{g},#{lat},#{long}"
+    def extract(f)
+      data = EXIFR::JPEG.new(f)
+      filename = File.basename(f)
+      e = data.exif?
+      g = !data.gps.nil?
+      lat = data.gps.latitude if data.exif? && !data.gps.nil?
+      long = data.gps.longitude if data.exif? && !data.gps.nil?
+      "#{filename},#{e},#{g},#{lat},#{long}"
+    end
   end
 
   desc 'extract_dir DIRECTORY', 'Extracts and prints info about all jpg files in a directory'
